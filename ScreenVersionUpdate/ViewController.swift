11fe31da1aa5af04a5cb6672f2a2ec6a8e5323b5
Lacie <dark830917@gmail.com>
2019-10-09 14:54:37 +0800

    all is completed. without demo and readme

--- ScreenVersionUpdate/ViewController.swift
+++ ScreenVersionUpdate/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        versionLabel.text = getNowVersion()
+        updateLaunchScreenAppVersion()
     }
-
-
+    
+    func getNowVersion() -> String {
+        let dictionary = Bundle.main.infoDictionary!
+        let version = dictionary["CFBundleShortVersionString"] as! String
+        
+        return "Version：\(version)"
+    }
+    
+    func updateLaunchScreenAppVersion() {
+        guard let launchScreen = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController() else {return}
+        self.view.addSubview(launchScreen.view)
+        
+        if let label = launchScreen.view.viewWithTag(1) as? UILabel {
+            label.text = getNowVersion()
+            UIView.animate(withDuration: 3,
+                           delay: 1,
+                           options: .curveEaseInOut,
+                           animations: {
+                            label.transform = CGAffineTransform(rotationAngle: .pi)
+                            label.transform = .identity
+                            launchScreen.view.alpha = 0
+            }) { (finished) in
+                launchScreen.view.removeFromSuperview()
+            }
+        }
+    }
 }
+
 
